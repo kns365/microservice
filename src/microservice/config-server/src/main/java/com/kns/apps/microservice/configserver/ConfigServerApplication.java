6f246bf59ebf9f1fe31a6e1fcd7d623b855ed509
cfcnndk <nnd.khoa@shinhanfinance.com.vn>
2024-05-10 13:45:55 +0700

    1

--- src/microservice/config-server/src/main/java/com/kns/apps/microservice/configserver/ConfigServerApplication.java
+++ src/microservice/config-server/src/main/java/com/kns/apps/microservice/configserver/ConfigServerApplication.java
@@ -2,7 +2,6 @@ package com.kns.apps.microservice.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
@@ -10,7 +9,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient        // It acts as a eureka client
 @EnableConfigServer
 public class ConfigServerApplication {
-//    DataSourceAutoConfiguration
+
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
     }
