227a4c57dc570f0a0a162e035c14f9a87ec47935
cfcnndk <nnd.khoa@shinhanfinance.com.vn>
2024-05-20 11:39:29 +0700

    kafka

---
+++ src/microservice/order-service/src/test/java/com/kns/apps/microservice/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kns.apps.microservice.orderservice;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
