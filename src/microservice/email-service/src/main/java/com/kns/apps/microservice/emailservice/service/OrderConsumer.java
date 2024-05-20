5923e38fcc4f07c29cffe103aa86aa22afe5b0ab
cfcnndk <nnd.khoa@shinhanfinance.com.vn>
2024-05-20 15:06:14 +0700

    temp kafka client

---
+++ src/microservice/email-service/src/main/java/com/kns/apps/microservice/emailservice/service/OrderConsumer.java
@@ -0,0 +1,23 @@
+package com.kns.apps.microservice.emailservice.service;
+
+import com.kns.apps.microservice.configserver.core.model.kafka.OrderEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderConsumer.class);
+
+    @KafkaListener(
+            topics = "${spring.kafka.topic.name}"
+            ,groupId = "${spring.kafka.consumer.group-id}"
+    )
+    public void consume(OrderEvent event){
+        LOGGER.info(String.format("Order event received in email service => %s", event.toString()));
+
+        // send an email to the customer
+    }
+}
